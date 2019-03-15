350e39b44462ee9b479bced1addbf98bff1b8a0f
陈鸿澍 <redtree@aliyun.com>
2019-03-15 15:20:58 +0800

    基础游戏框架，游戏路线提示，执行起子操作

---
+++ src/2pick1.love/conf.lua
@@ -0,0 +1,9 @@
+-- 游戏开始前加载 游戏的
+function love.conf(win)
+    win.title = '2pick1'
+    win.window.width=1440
+    win.window.height=900
+    win.modules.joystick = false
+    win.modules.physics = false
+end
+
